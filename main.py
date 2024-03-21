be1ede3ce8125551ab97529ba850cf3eb2ce2f68
Dennis Chan <dennisming2012@gmail.com>
2024-03-21 20:58:47 +0800

    Add cdk

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import sys
 import yfinance as yf
 
 def main():
-    ticker_symbol = sys.argv[1].upper()
-    ticker = yf.Ticker(ticker_symbol)
-    print("Last Price: $" + str(round(ticker.basic_info['lastPrice'], 2)))
+    splg = yf.Ticker("SPLG")
+    aapl = yf.Ticker("AAPL")
+    msft = yf.Ticker("MSFT")
+    print("SPLG Last Price: $" + str(round(splg.basic_info['lastPrice'], 2)))
+    print("AAPL Last Price: $" + str(round(aapl.basic_info['lastPrice'], 2)))
+    print("MSFT Last Price: $" + str(round(msft.basic_info['lastPrice'], 2)))
 
 if __name__ == "__main__":
     main()
