9bf96d0a4fd15c26feec52b0a960752f1cb233db
Dennis Chan <dennisming2012@gmail.com>
2024-03-24 16:57:59 +0800

    Add telegram bot

--- package/stoockie.py
+++ package/stoockie.py
@@ -1,21 +1,33 @@
-import yfinance as yf
 import os
+import requests
+import yfinance as yf
 
 
-def handler(event, context):
+def handler():
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+
     ticker_one = yf.Ticker(os.getenv('TICKER_ONE'))
     ticker_two = yf.Ticker(os.getenv('TICKER_TWO'))
     ticker_three = yf.Ticker(os.getenv('TICKER_THREE'))
     ticker_four = yf.Ticker(os.getenv('TICKER_FOUR'))
     ticker_five = yf.Ticker(os.getenv('TICKER_FIVE'))
 
-    print(ticker_one.ticker + " Last Price: " +
-          str(round(ticker_one.basic_info['lastPrice'], 2)))
-    print(ticker_two.ticker + " Last Price: " +
-          str(round(ticker_two.basic_info['lastPrice'], 2)))
-    print(ticker_three.ticker + " Last Price: " +
-          str(round(ticker_three.basic_info['lastPrice'], 2)))
-    print(ticker_four.ticker + " Last Price: " +
-          str(round(ticker_four.basic_info['lastPrice'], 2)))
-    print(ticker_five.ticker + " Last Price: " +
-          str(round(ticker_five.basic_info['lastPrice'], 2)))
+    printTicker(ticker_one)
+    printTicker(ticker_two)
+    printTicker(ticker_three)
+    printTicker(ticker_four)
+    printTicker(ticker_five)
+
+    message = "hello from telegram bot"
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+    return requests.get(url).json()
+
+
+def printTicker(ticker):
+    print(ticker.ticker + " Last Price: " +
+          str(round(ticker.basic_info['lastPrice'], 2)))
+
+
+if __name__:
+    handler()
